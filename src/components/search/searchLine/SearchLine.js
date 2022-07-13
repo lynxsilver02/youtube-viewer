2b172d605513a1065efa70d9b8e075b58be275ff
Alisa2Chev <ilya.baykov.1980@mail.ru>
2022-07-13 15:46:26 +0300

    JSON в Поиске , как работать с then(как достать оттуда значения ) ?

--- src/components/search/searchLine/SearchLine.js
+++ src/components/search/searchLine/SearchLine.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import './SearchLine.style.css'
+import "./SearchLine.style.css";
 
 
-export default function SearchLine() {
+const SearchLine = () => {
+  const [srch, setSrch] = useState("");
+  const hSubmit = (e) => {
+    e.preventDefault();
+    var msearh = srch;
+    console.log(msearh);
+    var API_KEY = "AIzaSyDU6uK4Q_gIQexdlSSX6IQCfe2FfzJbg_s";
+    videoSearch(API_KEY, msearh, 12);
+  };
+
+  function videoSearch(key, search, maxResulst) {
+    var video = "";
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?key=" +
+        key +
+        "&type=video&part=snippet&maxResults=" +
+        maxResulst +
+        "&q=" +
+        search
+    )
+      .then((res) => res.json())
+      .then((body) => console.log(body));
+    
+      
+  }
+
   return (
     <div className='searchLine'>
+      <form onSubmit={hSubmit}>
       <FaSearch className="icon" />
-      <input />
+        <input
+          type="text"
+          value={srch}
+          onChange={(e) => setSrch(e.target.value)}
+        />
+        <input className='searcsubmit' type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
+export default SearchLine;
