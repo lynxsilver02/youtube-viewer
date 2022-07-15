491577fd344bdf4c7a6336e4b92cf6efa6ce7e8f
Anastasia <lynxsilver02@gmail.com>
2022-07-15 08:37:06 +0300

    Added saving text on SearchLine

--- src/components/search/searchLine/SearchLine.js
+++ src/components/search/searchLine/SearchLine.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import './SearchLine.style.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchLineAction } from '../../../store/store';
 
+export let data = '0'
 
-export default function SearchLine() {
+export const SearchLine = (props) => {
+  const dispatch = useDispatch();
+  const searchLine = useSelector(state => state.searchLine);
+
+  const hSubmit = (e) => {
+    e.preventDefault();
+    var API_KEY = "AIzaSyA-wgJjwwqdfxyGP6q7AdgUrW0g933KJdE";
+    videoSearch(API_KEY, searchLine, 10);
+  };
+
+  const changeSearchLine = (searchLine) => {
+    dispatch(changeSearchLineAction(searchLine));
+  }
+
+  function videoSearch(key, search, maxResulst) {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?key=" +
+      key +
+      "&type=video&part=snippet&maxResults=" +
+      maxResulst +
+      "&q=" +
+      search
+    )
+      .then((res) => res.json())
+      .then((body) => { data = body; props.update();});
+  }
+
   return (
-    <div className='searchLine'>
-      <FaSearch className="icon" />
-      <input />
+    <div>
+      <form onSubmit={hSubmit} className='searchLine'>
+        <FaSearch className="icon" />
+        <input
+          type="text"
+          value={searchLine}
+          onChange={(e) => changeSearchLine(e.target.value)}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,30 @@
 import { createStore } from "redux"
 
 
-const defaultStateBookmarks = {
-  bookmarks: []
+const defaultState = {
+  bookmarks: [],
+  searchLine: ''
 }
 
 const ADD_BOOKMARK = "ADD_BOOKMARK"
 const REMOVE_BOOKMARK = "REMOVE_BOOKMARK"
+const CHANGE_SEARCH = "CHANGE_SEARCH"
 
-const reducerBookmarks = (state = defaultStateBookmarks, action) => {
+const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_BOOKMARK:
       return { ...state, bookmarks: [...state.bookmarks, action.payload] }
     case REMOVE_BOOKMARK:
       return { ...state, bookmarks: state.bookmarks.filter(bookmark => bookmark.id !== action.payload) }
+    case CHANGE_SEARCH:
+      return { ...state, searchLine: action.payload }
     default:
       return state;
   }
 }
 
-export const addBookmarkAction = (payload) => ({type: ADD_BOOKMARK, payload})
-export const removeBookmarkAction = (payload) => ({type: REMOVE_BOOKMARK, payload})
+export const addBookmarkAction = (payload) => ({ type: ADD_BOOKMARK, payload })
+export const removeBookmarkAction = (payload) => ({ type: REMOVE_BOOKMARK, payload })
+export const changeSearchLineAction = (payload) => ({ type: CHANGE_SEARCH, payload })
 
-export const store = createStore(reducerBookmarks);
+export const store = createStore(reducer);
